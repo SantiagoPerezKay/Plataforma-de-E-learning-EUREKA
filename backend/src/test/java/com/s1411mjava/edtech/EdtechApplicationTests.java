8c85767d11222d01f20c576ae288486e03cc8d8a
montexbjeliseo <secondmtx@gmail.com>
2024-03-31 20:47:42 -0300

    Backend Project added

---
+++ backend/src/test/java/com/s1411mjava/edtech/EdtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1411mjava.edtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
