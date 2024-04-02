3930cfaa16366197e962976158faf2b1a86b8888
Kobi96 <tobiasolveira@outlook.com>
2024-04-02 15:53:18 -0300

    creación Home y LogOut

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+/* Pages */
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+/* Components */
+import LogOutButton from "./components/LogOutButton/LogOutButton";
+
+export { Home, Login, Register, LogOutButton };
