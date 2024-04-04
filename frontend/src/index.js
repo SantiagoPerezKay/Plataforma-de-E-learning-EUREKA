06bfb3a09d96ac7309893980fae17aa6e53799b5
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-04-04 00:45:42 -0300

    se agrega un hero y un main provisorios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,5 +7,6 @@ import Register from "./pages/Register/Register";
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/navBar/NavBar";
 import LogOutButton from "./components/LogOutButton/LogOutButton";
+import HeroMain from "./components/heroMain/HeroMain";
 
-export { Home, Login, Register, LogOutButton, Footer, NavBar };
+export { Home, Login, Register, LogOutButton, Footer, NavBar , HeroMain};
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import Footer from "../../components/footer/Footer";
+import HeroMain from "../../components/heroMain/HeroMain";
 import NavBar from "../../components/navBar/NavBar";
 
 const Home = () => {
   return (
     <>
       <NavBar />
-    <div className="bg-gray-100 pt-12 h-60">
-      <h1 className="text-3xl text-center font-bold mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-      <h2 className="text-2xl text-center font-bold mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-    </div>
+      <HeroMain />
       <Footer />
     </>
   );
