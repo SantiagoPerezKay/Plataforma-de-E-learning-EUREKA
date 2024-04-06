f6821dce241a4861583193c45ee7f4c91d33f0fc
montexbjeliseo <secondmtx@gmail.com>
2024-04-06 02:24:11 -0300

    Enrollment entity added

---
+++ backend/src/main/java/com/s1411mjava/edtech/entity/Enrollment.java
@@ -0,0 +1,38 @@
+package com.s1411mjava.edtech.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "enrollments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    private Date creationDate;
+
+    private Integer qualification;
+}
