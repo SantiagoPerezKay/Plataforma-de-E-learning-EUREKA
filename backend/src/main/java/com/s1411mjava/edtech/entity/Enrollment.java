86d928bba5c5b1b86f150eb362eb05375176dec9
montexbjeliseo <secondmtx@gmail.com>
2024-04-10 16:55:46 -0300

    Add some enrollments

--- backend/src/main/java/com/s1411mjava/edtech/entity/Enrollment.java
+++ backend/src/main/java/com/s1411mjava/edtech/entity/Enrollment.java
@@ -1,10 +1,7 @@
 package com.s1411mjava.edtech.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
@@ -15,6 +12,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Enrollment {
 
     @Id
