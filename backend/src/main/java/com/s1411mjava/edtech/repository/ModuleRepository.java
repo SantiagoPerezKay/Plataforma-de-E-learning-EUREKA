e7d86762fa28e9436ebe01f448fd1eea5940633a
Claudia <claudia.ortiz@uc.cl>
2024-04-10 13:50:47 -0400

    feat: added CourseService, MyCourseDto, MyCourseMapper, MyCourseController and method for ModuleRepository.

--- backend/src/main/java/com/s1411mjava/edtech/repository/ModuleRepository.java
+++ backend/src/main/java/com/s1411mjava/edtech/repository/ModuleRepository.java
@@ -3,5 +3,9 @@ package com.s1411mjava.edtech.repository;
 import com.s1411mjava.edtech.entity.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ModuleRepository extends JpaRepository<Module, Long> {
+
+    List<Module> findByCourseId(Long courseId);
 }
