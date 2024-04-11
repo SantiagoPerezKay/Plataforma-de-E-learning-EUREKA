7e5bf55ec30d4e41714323845da3d09a17a5f4e8
Kobi96 <tobiasolveira@outlook.com>
2024-04-11 16:03:48 -0300

    front-back connection mis-cursos, inscripcion

--- frontend/src/api/course/index.js
+++ frontend/src/api/course/index.js
@@ -1,56 +1,60 @@
 import axios from "axios";
 
 const useCourse = () => {
+  const getCatalogCourses = async () => {
+    try {
+      const { data } = await axios(
+        "https://s14-11-m-java.onrender.com/api/v1/catalog/allcatalog"
+      );
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
 
+  const coursesByUser = async () => {
+    const token = localStorage.getItem("jwt");
 
-    const getCatalogCourses = async () => {
-        try {
-            const { data } = await axios("https://s14-11-m-java.onrender.com/api/v1/catalog/allcatalog");
-            return data
-        } catch (error) {
-            throw new Error(error.message);
-        }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     };
 
-    const coursesByUser = async () => {
-        const token = localStorage.getItem("jwt")
+    try {
+      const { data } = await axios(
+        "https://s14-11-m-java.onrender.com/api/v1/enrollments/students",
+        config
+      );
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
 
-        const config={
-            headers:{
-                'Content-Type':'application/json',
-                Authorization: `Bearer ${token}`
-            }
-        }
+  const courseById = async (id) => {
+    const token = localStorage.getItem("jwt");
 
-        
-        try {
-            const { data } = await axios("https://s14-11-m-java.onrender.com/api/v1/enrollments/students",config);
-            return data
-        }catch(error) {
-            throw new Error(error.message);
-        }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     };
 
-    const courseById = async (id) => {
-        const token = localStorage.getItem("jwt")
+    try {
+      const { data } = await axios(
+        `https://s14-11-m-java.onrender.com/api/v1/mycourses/mycourses/${id}`,
+        config
+      );
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
 
-        const config={
-            headers:{
-                'Content-Type':'application/json',
-                Authorization: `Bearer ${token}`
-            }
-        }
-
-        
-        try {
-            const { data } = await axios(`https://s14-11-m-java.onrender.com/api/v1/mycourses/mycourses/${id}`,config);
-            return data
-        }catch(error) {
-            throw new Error(error.message);
-        }
-    };
-
-    return { getCatalogCourses, coursesByUser, courseById};
+  return { getCatalogCourses, coursesByUser, courseById };
 };
 
 export default useCourse;
--- frontend/src/components/CursoCard/CursoCard.jsx
+++ frontend/src/components/CursoCard/CursoCard.jsx
@@ -1,38 +1,31 @@
 import starLine from "./star-line.svg";
 import starFill from "./star-fill.svg";
 import PropTypes from "prop-types";
-import cardimg5 from "../../pages/Landing/img/cardimg-negocios.webp";
 
 import { Link } from "react-router-dom";
 
 const CursoCard = (props) => {
   return (
     <>
-    <Link
-      to={'/dashboard/curso/1/4'}
-    >
-      <div className="w-full card-container flex flex-col items-center border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden">
-        <img src={cardimg5} className="w-full" />
-        <div className="stars flex my-2">
-          <img src={starFill} width="32" />
-          <img src={starFill} width="32" />
-          <img src={starFill} width="32" />
-          <img src={starFill} width="32" />
-          <img src={starLine} width="32" />
+      <Link to={"/dashboard/curso/1/4"}>
+        <div className="w-full card-container flex flex-col items-center border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden">
+          <img src={props.image} className="w-full" />
+          <div className="stars flex my-2">
+            <img src={starFill} width="32" />
+            <img src={starFill} width="32" />
+            <img src={starFill} width="32" />
+            <img src={starFill} width="32" />
+            <img src={starLine} width="32" />
+          </div>
+          <h3 className="font-bold text-center">{props.title}</h3>
         </div>
-        <h3 className="font-bold text-center">{props.title}</h3>
-        <p className="w-full text-justify text-sm px-8">{props.description}</p>
-        <button className="bg-blue-300 font-bold text-sm rounded-2xl my-4 px-4 py-2 border border-blue400 shadow-md shadow-slate-400">
-          Inscríbete
-        </button>
-      </div>
-    </Link>
+      </Link>
     </>
   );
 };
 CursoCard.propTypes = {
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
 };
 
 export default CursoCard;
