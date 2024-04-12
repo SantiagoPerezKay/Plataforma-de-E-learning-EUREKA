a72e4ebf2e7179f75cd5d7e303061e7a8fdd55ac
Jaime A <jaimeangel1097@gmail.com>
2024-04-11 20:23:19 -0500

    agregando endpoint a landing page, catalogo de cursos

--- frontend/src/pages/Landing/LandingPage.jsx
+++ frontend/src/pages/Landing/LandingPage.jsx
@@ -1,3 +1,8 @@
+import { useEffect } from "react";
+
+import CursoCard from "../../components/CursoCard/CursoCard";
+import Spinner from "../../components/Loading/Spinner"
+
 import image1 from "./img/image1.png";
 import image2 from "./img/image2.png";
 import image3 from "./img/image3.png";
@@ -22,18 +27,45 @@ import starLine from "./img/star-line.svg";
 import starFill from "./img/star-fill.svg";
 import { useNavigate } from "react-router-dom";
 
+import useCourse from "../../api/course/index";
+import { useState } from "react";
+
+import ElegirElementosRandom from "../../utils/ElegirElementosRandom";
+
 function LandingPage() {
-    const navigate = useNavigate();
-    const handleClick = () =>{
-      if(localStorage.getItem('jwt')){
-        navigate("/dashboard/student")
-      } else {
-        navigate("/register")
+  const navigate = useNavigate();
+  const [catalogo,setCatalogo]=useState([])
+
+  const {
+    getCatalogCourses
+  }=useCourse()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const catalogCourses = await getCatalogCourses();
+        const newCatalog = ElegirElementosRandom(catalogCourses,5)
+        setCatalogo(newCatalog);
+
+      } catch (error) {
+        console.error("Error fetching courses:", error);
       }
+    };
+
+    fetchData();
+  },[]);
+    
+  const handleClick = () =>{
+    if(localStorage.getItem('jwt')){
+      navigate("/dashboard/student")
+    } else {
+      navigate("/register")
     }
+  }
+
   return (
     <>
-      <div className="hero h-80 flex justify-center w-full 2xl:w-3/4 2xl:mx-auto overflow-hidden mb-20">
+      <div className="hero h-80 flex justify-center w-full 2xl:w-3/4 2xl:mx-auto overflow-hidden mb-12">
         <div className="hero-left w-100 flex flex-col justify-center pl-8 max-lg:flex-1">
           <h1 className="text-3xl font-bold mb-4">
             <span className="text-blue-600">Cursos y aprendizaje en línea </span><br /> en todo el mundo
@@ -75,99 +107,19 @@ function LandingPage() {
         </div>
       </div>
       <div className="main">
-        <div className="row-container flex flex-wrap gap-y-8 justify-between max-lg:justify-evenly mb-20 lg:w-[1024px] lg:mx-auto">
-          <div className="card-container flex flex-col items-center w-72 border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden max-lg:mx-auto">
-            <img src={cardimg5} className="w-full" />
-            <div className="stars flex my-2">
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starLine} width="32" />
-            </div>
-            <h3 className="font-bold text-center">Negocios</h3>
-            <p className="text-sm w-72 px-8">
-              Estos cursos están diseñados para proporcionarte una base sólida
-              en diferentes aspectos del mundo empresarial.
-            </p>
-            <button className="bg-blue-300 font-bold text-sm rounded-2xl my-4 px-4 py-2 border border-blue400 shadow-md shadow-slate-400">
-              Inscríbete
-            </button>
-          </div>
-          <div className="card-container flex flex-col items-center w-72 border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden max-lg:mx-auto">
-            <img src={cardimg2} className="w-full" />
-            <div className="stars flex my-2">
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starLine} width="32" />
-            </div>
-            <h3 className="font-bold text-center">Finanzas</h3>
-            <p className="text-sm w-72 px-8">
-              Estos cursos te proporcionarán una base sólida de los conceptos y
-              herramientas fundamentales en el campo de las finanzas.
-            </p>
-            <button className="bg-blue-300 font-bold text-sm rounded-2xl my-4 px-4 py-2 border border-blue400 shadow-md shadow-slate-400">
-              Inscríbete
-            </button>
-          </div>
-          <div className="card-container flex flex-col items-center w-72 border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden max-lg:mx-auto">
-            <img src={cardimg3} className="w-full" />
-            <div className="stars flex my-2">
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starLine} width="32" />
-            </div>
-            <h3 className="font-bold text-center">IT</h3>
-            <p className="text-sm w-72 px-8">
-              Te proporcionarán bases en diferentes áreas de la tecnología de la
-              información, de la programación y el desarrollo web hasta la
-              ciencia de datos.
-            </p>
-            <button className="bg-blue-300 font-bold text-sm rounded-2xl my-4 px-4 py-2 border border-blue400 shadow-md shadow-slate-400">
-              Inscríbete
-            </button>
-          </div>
-          <div className="card-container flex flex-col items-center w-72 border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden max-lg:mx-auto">
-            <img src={cardimg4} className="w-full" />
-            <div className="stars flex my-2">
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starLine} width="32" />
-            </div>
-            <h3 className="font-bold text-center">Marketing</h3>
-            <p className="text-sm w-72 px-8">
-              Compenderás los conceptos y las herramientas fundamentales, desde
-              el marketing digital hasta el marketing de contenidos.
-            </p>
-            <button className="bg-blue-300 font-bold text-sm rounded-2xl my-4 px-4 py-2 border border-blue400 shadow-md shadow-slate-400">
-              Inscríbete
-            </button>
-          </div>
-          <div className="card-container flex flex-col items-center w-72 border border-gray-300 rounded-xl shadow-xl shadow-slate-300 cursor-pointer overflow-hidden max-lg:mx-auto">
-            <img src={cardimg1} className="w-full" />
-            <div className="stars flex my-2">
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starFill} width="32" />
-              <img src={starLine} width="32" />
-            </div>
-            <h3 className="font-bold text-center">Diseño</h3>
-            <p className="text-sm w-72 px-8">
-              Obtendrás una base desde el diseño gráfico a la experiencia de
-              usuario, donde desarrollarás tus habilidades teóricas y prácticas.
-            </p>
-            <button className="bg-blue-300 font-bold text-sm rounded-2xl my-4 px-4 py-2 border border-blue400 shadow-md shadow-slate-400">
-              Inscríbete
-            </button>
-          </div>
-          <div className="w-72 max-sm:hidden"></div>
+        <div className="grid px-10 py-10 gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:px-24 lg:py-10 lg:gap-12">
+          {
+            catalogo.length !== 0 ?
+              catalogo?.map((data) => (
+                <CursoCard
+                  key={data.id}
+                  title={data.title}
+                  image={data.image}
+                />
+              ))
+            :
+              <Spinner/>
+          }
         </div>
       </div>
     </>
