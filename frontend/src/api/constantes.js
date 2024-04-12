506db9fd031aa45c1c0d2b612c1c010b1ffdf4a7
Jaime A <jaimeangel1097@gmail.com>
2024-04-11 22:14:04 -0500

    agregando constantes de ruta API

---
+++ frontend/src/api/constantes.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://s14-11-m-java.onrender.com/api/v1/'
+/* CATALOG */
+const CATALOG = 'catalog/allcatalog'
+/* COURSE BY USER */
+const COURSES_BY_USER = 'enrollments/students'
+/* GET COURSE BY ID */
+const COURSE_BY_ID = 'mycourses/mycourses'
+/* LOGIN */
+const LOGIN = 'auth/login'
+/* REGISTER */
+const REGISTER = 'auth/register'
+
+export {
+    BASE_URL,
+    CATALOG,
+    COURSES_BY_USER,
+    COURSE_BY_ID,
+    LOGIN,
+    REGISTER
+}
