96220492a25e36f4e72ada2bd0d6763dd5652952
Mariana Cuba <118843955+MarianaCuba@users.noreply.github.com>
2024-04-15 16:55:41 -0300

    endpoint para obtener las categorias

--- backend/src/main/java/com/s1411mjava/edtech/controller/CatalogController.java
+++ backend/src/main/java/com/s1411mjava/edtech/controller/CatalogController.java
@@ -2,8 +2,12 @@ package com.s1411mjava.edtech.controller;
 
 
 import com.s1411mjava.edtech.dtos.CatalogDto;
+import com.s1411mjava.edtech.dtos.CategoryDto;
 import com.s1411mjava.edtech.dtos.EnrollmentDto;
+import com.s1411mjava.edtech.entity.Category;
+import com.s1411mjava.edtech.repository.CategoryRepository;
 import com.s1411mjava.edtech.service.impl.CatalogServiceImpl;
+import com.s1411mjava.edtech.service.impl.CategoryServiceImpl;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +25,16 @@ import java.util.List;
 @RequestMapping("/catalog")
 public class CatalogController {
     private final CatalogServiceImpl catalogServiceImpl;
+    private final CategoryServiceImpl categoryService;
     @GetMapping
 
     public ResponseEntity<List<CatalogDto>> findAllCatalog(){
         return ResponseEntity.ok(catalogServiceImpl.findAllCatalog());
 
+    }
+    @GetMapping("/categories")
+    public ResponseEntity<List<CategoryDto>> findAllCategories() {
+        List<CategoryDto> categoryDtos = categoryService.findAllCategories();
+        return ResponseEntity.ok(categoryDtos);
     }
 }
