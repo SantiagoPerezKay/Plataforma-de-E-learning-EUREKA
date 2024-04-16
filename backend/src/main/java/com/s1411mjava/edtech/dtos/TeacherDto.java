ab2a20033c1f56105808296fc1e05c7128551989
LucianaMazur <lucianamazur62@gmail.com>
2024-04-16 20:00:47 -0300

    Cambios en campos de la entidad teacher

--- backend/src/main/java/com/s1411mjava/edtech/dtos/TeacherDto.java
+++ backend/src/main/java/com/s1411mjava/edtech/dtos/TeacherDto.java
@@ -1,13 +1,16 @@
 package com.s1411mjava.edtech.dtos;
 
+import jakarta.persistence.Column;
 import lombok.Data;
 @Data
 public class TeacherDto {
     private Long id;
     private String experience;
     private String credentials;
-    private String info;
     private Long user_id;
+    private String linkedin;
+    private String file;
+
 
 
 }
