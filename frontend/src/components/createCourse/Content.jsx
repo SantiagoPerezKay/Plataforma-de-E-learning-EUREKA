4c27ba5807b3f1d565c034f63bb44731a7ef51b6
Jaime A <jaimeangel1097@gmail.com>
2024-04-18 12:56:39 -0500

    agregando estilos y validaciones

--- frontend/src/components/createCourse/Content.jsx
+++ frontend/src/components/createCourse/Content.jsx
@@ -84,7 +84,7 @@ function Content() {
                 </button>
                 <h1 className="text-xl font-bold">{`Modulo: ${tituloModulo}`}</h1>
                 <button
-                    onClick={()=>setOpen(prev => !prev)}
+                    onClick={()=>setOpen(true)}
                     className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
                     CREAR CONTENIDO
@@ -133,13 +133,20 @@ function Content() {
                             label={'Subir archivo'}
                         />
                     </div>
-                    
-                    <button
-                        onClick={handleCrearContenido}
-                        className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    >
-                        CREAR
-                    </button>
+                    <div>
+                        <button
+                            onClick={handleCrearContenido}
+                            className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            CREAR
+                        </button>
+                        <button
+                            onClick={()=>setOpen(false)}
+                            className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            CANCELAR
+                        </button>
+                    </div>
                 </div>
             }
 
--- frontend/src/components/createCourse/Modulos.jsx
+++ frontend/src/components/createCourse/Modulos.jsx
@@ -44,7 +44,7 @@ function Modulos({
         <div className="w-full">
             <div className="flex flex-row justify-between">
                 <button
-                    onClick={()=>setOpen(prev => !prev)}
+                    onClick={()=>setOpen(true)}
                     className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
                     CREAR MODULO
@@ -69,12 +69,21 @@ function Modulos({
                         type="text" 
                         className="border rounded shadow py-1 px-5 outline-none"
                     />
-                    <button
-                        onClick={handleCrearModulo}
-                        className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    >
-                        CREAR
-                    </button>
+                    <div>
+                        <button
+                            onClick={handleCrearModulo}
+                            className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            CREAR
+                        </button>
+                        <button
+                            onClick={()=>setOpen(false)}
+                            className="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            CANCELAR
+                        </button>
+
+                    </div>
                 </div>
             }
 
