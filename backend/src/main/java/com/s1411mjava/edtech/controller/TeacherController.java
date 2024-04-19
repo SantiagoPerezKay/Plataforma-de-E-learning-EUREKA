607ccdbd53c78465cc0bfb15accca7992e1ffb36
montexbjeliseo <secondmtx@gmail.com>
2024-04-18 22:28:40 -0300

    Fix validate modules and contents

--- backend/src/main/java/com/s1411mjava/edtech/controller/TeacherController.java
+++ backend/src/main/java/com/s1411mjava/edtech/controller/TeacherController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -38,6 +39,7 @@ public class TeacherController {
     @PreAuthorize("hasRole('ROLE_TEACHER')")
     public ResponseEntity<CreatedCourseDTO> createCourse(@Valid @RequestBody CreateCourseDTO createCourseDTO) {
         CreatedCourseDTO createdCourseDTO = teacherService.createCourse(createCourseDTO);
+
         return new ResponseEntity<>(createdCourseDTO, HttpStatus.CREATED);
     }
 
--- backend/src/main/java/com/s1411mjava/edtech/service/impl/TeacherServiceImpl.java
+++ backend/src/main/java/com/s1411mjava/edtech/service/impl/TeacherServiceImpl.java
@@ -2,9 +2,11 @@ package com.s1411mjava.edtech.service.impl;
 import com.s1411mjava.edtech.dtos.*;
 import com.s1411mjava.edtech.entity.*;
 import com.s1411mjava.edtech.entity.Module;
+import com.s1411mjava.edtech.exception.MandatoryFieldException;
 import com.s1411mjava.edtech.exception.ResourceNotFoundException;
 import com.s1411mjava.edtech.mapper.CatalogMapper;
 import com.s1411mjava.edtech.mapper.CourseModuleMapper;
+import com.s1411mjava.edtech.mapper.ModuleMapper;
 import com.s1411mjava.edtech.repository.*;
 import com.s1411mjava.edtech.service.TeacherService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,6 +37,9 @@ public class TeacherServiceImpl implements TeacherService {
     private final UserRepository userRepository;
 
     private final CourseModuleMapper courseMapper;
+    private final ModuleMapper moduleMapper;
+
+    private final Validator validator;
 
     @Override
     public TeacherOutDto createTeacher(TeacherInDto teacherInDto) {
@@ -49,7 +59,8 @@ public class TeacherServiceImpl implements TeacherService {
 
 
     @Override
-    public CreatedCourseDTO createCourse(CreateCourseDTO createCourseDTO) {
+    @Validated
+    public CreatedCourseDTO createCourse(CreateCourseDTO createCourseDTO){
 
         Teacher teacher = getTeacherByUser(getCurrentUser());
 
@@ -57,7 +68,7 @@ public class TeacherServiceImpl implements TeacherService {
 
         Course savedCourse = createCourseFromDTO(createCourseDTO, teacher, category);
 
-        savedCourse.setModules(createModules(savedCourse, savedCourse.getModules()));
+        savedCourse.setModules(createModules(savedCourse, createCourseDTO.getModules()));
 
         savedCourse = courseRepository.save(savedCourse);
 
@@ -70,9 +81,38 @@ public class TeacherServiceImpl implements TeacherService {
                 .orElseThrow(() -> new ResourceNotFoundException("Categoría no encontrada"));
     }
 
-    private List<Module> createModules(Course savedCourse, List<Module> modules) {
-        modules.forEach(module -> module.setCourse(savedCourse));
+    private List<Module> createModules(Course savedCourse, List<CreateCourseModuleDTO> moduleDTOs) {
+
+        List<Module> modules = new ArrayList<>();
+
+        moduleDTOs.forEach(module -> {
+
+            validateModules(module);
+
+            Module moduleEntity = moduleMapper.toEntity(module);
+            moduleEntity.setCourse(savedCourse);
+            modules.add(moduleEntity);
+        });
         return moduleRepository.saveAll(modules);
+    }
+
+    private void validateModules(CreateCourseModuleDTO module) {
+        BindingResult errors = new BeanPropertyBindingResult(module, "moduleDTO");
+        validator.validate(module, errors);
+        if (errors.hasErrors()) {
+            throw new MandatoryFieldException("Validaciones fallidas", errors);
+        }
+        validateContents(module.getContents());
+    }
+
+    private void validateContents(List<CreateCourseContentDTO> contents) {
+        contents.forEach(content -> {
+            BindingResult errors = new BeanPropertyBindingResult(content, "contents");
+            validator.validate(content, errors);
+            if (errors.hasErrors()) {
+                throw new MandatoryFieldException("Validaciones fallidas", errors);
+            }
+        });
     }
 
     private Teacher getTeacherByUser(User user) {
