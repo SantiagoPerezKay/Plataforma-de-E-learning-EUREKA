dd60ad498f7e79940f4f984ea3bed14f50142cbd
montexbjeliseo <secondmtx@gmail.com>
2024-04-18 22:30:51 -0300

    Remove unused imports

--- backend/src/main/java/com/s1411mjava/edtech/controller/TeacherController.java
+++ backend/src/main/java/com/s1411mjava/edtech/controller/TeacherController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- backend/src/main/java/com/s1411mjava/edtech/mapper/ModuleMapper.java
+++ backend/src/main/java/com/s1411mjava/edtech/mapper/ModuleMapper.java
@@ -3,7 +3,6 @@ package com.s1411mjava.edtech.mapper;
 import com.s1411mjava.edtech.dtos.CreateCourseModuleDTO;
 import com.s1411mjava.edtech.entity.Module;
 import org.mapstruct.Mapper;
-import org.springframework.validation.annotation.Validated;
 
 @Mapper(componentModel = "spring")
 public interface ModuleMapper {
