ba672f5c77155bb312b543588e64da87dad4f1f1
Kobi96 <tobiasolveira@outlook.com>
2024-04-22 15:26:22 -0300

    images

--- frontend/src/pages/Equipo/helper.js
+++ frontend/src/pages/Equipo/helper.js
@@ -1,104 +1,120 @@
+import sandra from "./img/SandraRodriguez.png";
+import fer from "./img/FernandoVergel.png";
+import emi from "./img/EmilianoLongo.png";
+import jaime from "./img/JaimeAngel.png";
+import gaston from "./img/GastonSalas.png";
+import tobi from "./img/TobiasOlveira.png";
+import luz from "./img/LuzTabraj.png";
+import tomas from "./img/TomasSoto.png";
+import eliseo from "./img/EliseoMontenegro.png";
+import santiago from "./img/SantiagoPerez.png";
+import mariana from "./img/MarianaCuba.png";
+import claudia from "./img/ClaudiaOrtizChi.png";
+import luciana from "./img/LucianaMazur.png";
+import leandro from "./img/LeandroDeferrari.png";
+import alejandra from "./img/AlejandraPanizza.png";
+
 export const equipo = [
   {
-    foto: "/public/img/SandraRodriguez.png",
+    foto: sandra,
     nombre: "Sandra Rodriguez",
     rol: "Project Manager",
     linkedin: "https://www.linkedin.com/in/sandra-rodriguez-133687275/",
     pais: "Colombia",
   },
   {
-    foto: "/public/img/FernandoVergel.png",
+    foto: fer,
     nombre: "Fernando Vergel",
     rol: "UX/UI Designer",
     linkedin: "https://www.linkedin.com/in/fernandovergel/",
     pais: "Peru",
   },
   {
-    foto: "/public/img/EmilianoLongo.png",
+    foto: emi,
     nombre: "Emiliano Joaquín Longo",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/emilianojlongo/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/JaimeAngel.png",
+    foto: jaime,
     nombre: "Jaime Orlando Angel Barreto",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/jaimeangeldev/",
     pais: "Colombia",
   },
   {
-    foto: "/public/img/GastonSalas.png",
+    foto: gaston,
     nombre: "Gaston Salas",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/gastonsalas095/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/TobiasOlveira.png",
+    foto: tobi,
     nombre: "Tobias Olveira",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/tobias-olveira-52a0311b7/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/LuzTabraj.png",
+    foto: luz,
     nombre: "Luz Tabraj Briceño",
     rol: "QA Tester",
     linkedin: "https://www.linkedin.com/in/luz-tabraj/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/TomasSoto.png",
+    foto: tomas,
     nombre: "Tomás Soto",
     rol: "QA Tester",
     linkedin: "https://www.linkedin.com/in/tom%C3%A1s-soto-038709267/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/EliseoMontenegro.png",
+    foto: eliseo,
     nombre: "Eliseo Montenegro",
     rol: "Backend Developer",
     linkedin: "https://www.linkedin.com/in/eliseo-montenegro/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/MarianaCuba.png",
+    foto: mariana,
     nombre: "Mariana Cuba",
     rol: "Backend Developer",
     linkedin: "https://www.linkedin.com/in/mariana-cuba-72a901258/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/SantiagoPerez.png",
+    foto: santiago,
     nombre: "Santiago perez kay",
     rol: "Backend Developer",
     linkedin: "https://www.linkedin.com/in/santiagoperezkay/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/ClaudiaOrtizChi.png",
+    foto: claudia,
     nombre: "Claudia Ortiz",
     rol: "Backend Developer",
     linkedin: "https://www.linkedin.com/in/claudia-ortiz-backend/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/LucianaMazur.png",
+    foto: luciana,
     nombre: "Luciana Mazur",
     rol: "Backend Developer",
     linkedin: "https://www.linkedin.com/in/lucianamazur/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/LeandroDeferrari.png",
+    foto: leandro,
     nombre: "Leandro Deferrari Arevalo",
     rol: "Backend Developer",
     linkedin: "https://www.linkedin.com/in/leandrodeferrari/",
     pais: "Argentina",
   },
   {
-    foto: "/public/img/AlejandraPanizza.png",
+    foto: alejandra,
     nombre: "Alejandra Carla Panizza",
     rol: "Team Leader",
     linkedin: "https://www.linkedin.com/in/alejandra-carla-panizza/",
