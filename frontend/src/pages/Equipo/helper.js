bacc0425b069af99f3f5e0a64f837b37bfb60e73
Kobi96 <tobiasolveira@outlook.com>
2024-04-22 15:13:51 -0300

    images

--- frontend/src/pages/Equipo/helper.js
+++ frontend/src/pages/Equipo/helper.js
@@ -1,48 +1,64 @@
+import sandra from "./img/SandraRodriguez.png";
+import fer from "./img/FernandoVergel.png";
+import emi from "./img/EmilianoLongo.png";
+import jaime from "./img/JaimeAngel.png";
+import gaston from "./img/GastonSalas.png";
+import tobi from "./img/TobiasOlveira.png";
+import luz from "./img/LuzTabraj.png";
+/* import tomas from "./img/hero12.png";
+import eliseo from "./img/hero13.png";
+import santiago from "./img/hero14.png";
+import mariana from "./img/hero15.png";
+import claudia from "./img/hero16.png";
+import luciana from "./img/hero22.png";
+import leandro from "./img/hero23.png";
+import alejandra from "./img/hero24.png"; */
+
 export const equipo = [
   {
-    foto: "/public/SandraRodriguez.png",
+    foto: sandra,
     nombre: "Sandra Rodriguez",
     rol: "Project Manager",
     linkedin: "https://www.linkedin.com/in/sandra-rodriguez-133687275/",
     pais: "Colombia",
   },
   {
-    foto: "/public/FernandoVergel.png",
+    foto: fer,
     nombre: "Fernando Vergel",
     rol: "UX/UI Designer",
     linkedin: "https://www.linkedin.com/in/fernandovergel/",
     pais: "Peru",
   },
   {
-    foto: "/public/EmilianoLongo.png",
+    foto: emi,
     nombre: "Emiliano Joaquín Longo",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/emilianojlongo/",
     pais: "Argentina",
   },
   {
-    foto: "/public/JaimeAngel.png",
+    foto: jaime,
     nombre: "Jaime Orlando Angel Barreto",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/jaimeangeldev/",
     pais: "Colombia",
   },
   {
-    foto: "/public/GastonSalas.png",
+    foto: gaston,
     nombre: "Gaston Salas",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/gastonsalas095/",
     pais: "Argentina",
   },
   {
-    foto: "/public/TobiasOlveira.png",
+    foto: tobi,
     nombre: "Tobias Olveira",
     rol: "Frontend Developer",
     linkedin: "https://www.linkedin.com/in/tobias-olveira-52a0311b7/",
     pais: "Argentina",
   },
   {
-    foto: "/public/LuzTabraj.png",
+    foto: luz,
     nombre: "Luz Tabraj Briceño",
     rol: "QA Tester",
     linkedin: "https://www.linkedin.com/in/luz-tabraj/",
